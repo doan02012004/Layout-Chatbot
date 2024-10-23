efc6fefab11b2a43887968bd3a7418ea883bc1b0
doan02012004 <doana1k24@gmail.com>
2024-10-23 13:51:50 +0700

    second commit

---
+++ src/pages/(home)/Page.tsx
@@ -0,0 +1,13 @@
+
+import Button from '../../components/buttons/Button'
+
+const HomePage = () => {
+  return (
+    <>
+        <div className=' font-bold '>HomePage</div>
+        <Button buttonText='Click me' buttonType='primary'/>
+    </>
+  )
+}
+
+export default HomePage
